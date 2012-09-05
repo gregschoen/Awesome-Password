233b0c82970b9f090a7fba80bff66dd792d4c1ab
Greg Schoen <greg.schoen@gmail.com>
2012-09-05 00:59:50 -0500

    Cleaning up logic and adding config file generation

--- AwesomePassword.class.php
+++ AwesomePassword.class.php
@@ -2,13 +2,11 @@
 
 class AwesomePassword
 {
-	private static $PRE_PEPPER = "f9b63727bde54311b4e4756020135c55";
-	private static $POST_PEPPER = "b8741042f290483abe5713c93a63efc0";
-	private static $ROUNDS = 123456;
+	static $config;
 
 	static function hash($password)
 	{
-		$settings = '$6$rounds=' . self::$ROUNDS . '$';
+		$settings = '$6$rounds=' . self::config()->rounds . '$';
 		$password = self::pepper($password);
 		$hash = crypt($password, $settings . self::salt() . '$');
 		$light_hash = str_replace($settings,'',$hash);
@@ -17,42 +15,51 @@ class AwesomePassword
 
 	static function check($password, $light_hash)
 	{
-		$hash = '$6$rounds=' . self::$ROUNDS . '$' . $light_hash;
+		$hash = '$6$rounds=' . self::config()->rounds . '$' . $light_hash;
 		$password = self::pepper($password);
 		return crypt($password, $hash) == $hash;
 	}
 
-	static function uuid()
-	{
-		// http://www.php.net/manual/en/function.uniqid.php#94959
-		return sprintf("%04x%04x%04x%04x%04x%04x%04x%04x",
-			mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-			mt_rand(0, 0xffff),
-			mt_rand(0, 0x0fff) | 0x4000,
-			mt_rand(0, 0x3fff) | 0x8000,
-			mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-		);
-	}
-
 	static function pepper($password)
 	{
-		return self::$PRE_PEPPER . $password . self::$POST_PEPPER;
+		return self::config()->pre_pepper . $password . self::config()->post_pepper;
 	}
 
 	static function salt()
 	{
 		return substr(base64_encode(self::uuid()),0,16);
 	}
+
+	static function config()
+	{
+		if(!self::$config)
+		{
+			if(file_exists("config.json"))
+			{
+				self::$config = json_decode(file_get_contents("config.json"));
+			}
+			else
+			{
+				echo "Saving unique hashing values to config.json\n";
+				self::$config = (object) array(
+					"pre_pepper" => self::uuid(),
+					"post_pepper" => self::uuid(),
+					"rounds" => mt_rand(120000,140000)
+				);
+				file_put_contents("config.json",json_encode(self::$config));
+			}
+		}
+
+		return self::$config;
+	}
+
+	static function uuid()
+	{
+		// http://www.php.net/manual/en/function.uniqid.php#94959
+		return sprintf("%04x%04x%04x%04x%04x%04x%04x%04x",
+			mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+			mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000,
+			mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+		);
+	}
 }
-
-// $password = "password";
-// $hash = AwesomePassword::hash($password);
-
-// "ODdmNDAyYTAzOWU2$/x7uWSqyRZSPKvDEkRqO/Fc/z8ihnxeeMLhHpnxAwY6MEXMcsP11fu3.Dtm/UIYuJyi8fYvTzMVtwEvyvSJzF/";
-
-// $check = AwesomePassword::check($password,$hash);
-
-// if($check)
-// {
-// 	echo "Hash validated\n";
-// }
--- README.md
+++ README.md
@@ -9,10 +9,20 @@ I constantly read stories about X or Y site's users tables getting hacked and th
 Perhaps you're using unsalted MD5 or SHA1, but it's practically the same depending on the password. For example, take the password of "awesome" or "password": 
 
 - MD5(awesome): [be121740bf988b2225a313fa1f107ca1](http://www.md5-lookup.com/index.php?q=be121740bf988b2225a313fa1f107ca1)
-- SHA1(awesome): [03d67c263c27a453ef65b29e30334727333ccbcd](http://www.sha1-lookup.com/index.php?q=03d67c263c27a453ef65b29e30334727333ccbcd)
-
 - MD5(password): [5f4dcc3b5aa765d61d8327deb882cf99](http://www.md5-lookup.com/index.php?q=5f4dcc3b5aa765d61d8327deb882cf99)
-- SHA1(password): [5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8](http://www.sha1-lookup.com/index.php?q=5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8)
 
-Still think that your "hashed" passwords are safe?
+Still think that your "hashed" passwords are safe? Enter AwesomePassword:
 
+```
+$password = "password";
+$hash = AwesomePassword::hash($password);
+
+// "ODdmNDAyYTAzOWU2$/x7uWSqyRZSPKvDEkRqO/Fc/z8ihnxeeMLhHpnxAwY6MEXMcsP11fu3.Dtm/UIYuJyi8fYvTzMVtwEvyvSJzF/";
+
+$check = AwesomePassword::check($password,$hash);
+
+if($check)
+{
+	echo "Hash validated\n";
+}
+```
